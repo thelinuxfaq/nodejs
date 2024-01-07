95f8110dea52a524788dedd19672e95323051f88
THELINUXFAQ <thelinuxfaq@gmail.com>
2024-01-07 20:38:49 +0530

    Update index.js
    new commit1

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello Node-testing, Welcome to the Community!\n'
+  const msg = 'Hello TechnoEdge, Welcome to the Community!\n'
   res.end(msg);
 });
 
